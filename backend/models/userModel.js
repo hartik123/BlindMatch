15eb3c7f0cd938d47f3b983262e0cfe803ed0fe0
Glen <dsouzaglen30@gmail.com>
2023-03-04 12:29:08 +0530

    OTP authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name: {type:String, required: true},
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    mobile: { type: Number, required: true },
+    mobOtp: { type: Number, required: false },
     email: {type:String, required: true},
-    password: {type:String, required: true},
-    isAdmin: {type:Boolean, default: false, required:true},
-}, {
-    timestamps: true
-})
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true },
+    isVerified: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userModel = mongoose.model('users', userSchema);
+const userModel = mongoose.model("users", userSchema);
 
 module.exports = userModel;
--- backend/routes/home.js
+++ backend/routes/home.js
@@ -3,11 +3,12 @@ const router = express.Router();
 // const { checkCred } = require("../utils/checkCred");
 // const { sendEmailOtp } = require("../utils/emailOtp");
 // const { sendMobileOtp } = require("../utils/mobileOtp");
-const {login, register, getUserInfo} = require('../controllers/userControllers');
+const {login, register, verify, getUserInfo} = require('../controllers/userControllers');
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/verify", verify);
 
 router.get('/getuserinfo', authMiddleware, getUserInfo);
 
