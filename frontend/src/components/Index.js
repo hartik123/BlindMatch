57fed327a3106395f8f7706d06cd10b534783eba
Glen <dsouzaglen30@gmail.com>
2023-03-05 11:00:46 +0530

    homepage

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import ChatArea from './ChatArea'
-import UserSearch from './UserSearch'
-import UsersList from './UsersList'
-import {io} from 'socket.io-client'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import ChatArea from "./ChatArea";
+import UserSearch from "./UserSearch";
+import UsersList from "./UsersList";
+import startChat from "../assets/images/startChat.png";
+import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000")
+const socket = io("http://localhost:4000");
 const Index = () => {
-    const [searchKey, setSearchKey]= useState('')
-    const {selectedChat,user} = useSelector((state)=>state.users)
-    const [onlineUsers, setOnlineUsers] = useState([]);
-    useEffect(()=>{
-         // join the room
+  const [searchKey, setSearchKey] = useState("");
+  const { selectedChat, user } = useSelector((state) => state.users);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  useEffect(() => {
+    // join the room
     if (user) {
       socket.emit("join-room", user._id);
       // socket.emit("came-online", user._id);
@@ -20,32 +21,37 @@ const Index = () => {
       //   setOnlineUsers(users);
       // });
     }
-   },[user])
+  }, [user]);
 
   return (
-    <div className='flex gap-5'>
-        <div className='w-96'>
-            <UserSearch  searchKey={searchKey} setSearchKey={setSearchKey} />
-            <UsersList searchKey={searchKey} socket={socket}
-          onlineUsers={onlineUsers}/>
+    <div className="flex gap-5">
+      <div className="w-96">
+        <UserSearch searchKey={searchKey} setSearchKey={setSearchKey} />
+        <UsersList
+          searchKey={searchKey}
+          socket={socket}
+          onlineUsers={onlineUsers}
+        />
+      </div>
+      {selectedChat && (
+        <div className="w-full">
+          <ChatArea socket={socket} />
         </div>
-        {selectedChat && <div className='w-full'>
-            <ChatArea socket={socket}/>
-        </div>}
-        {!selectedChat && (
+      )}
+      {!selectedChat && (
         <div className="w-full h-[80vh]  items-center justify-center flex bg-white flex-col">
           <img
-            src="https://www.pngmart.com/files/16/Speech-Chat-Icon-Transparent-PNG.png"
-            alt=""
-            className="w-96 h-96"
+            src={startChat}
+            alt="ChatImg"
+            style={{ width: "75%", height: "70%" }}
           />
-          <h1 className="text-2xl font-semibold text-gray-500">
-            Select a user to chat
-          </h1>
+          <div className="btn btn-outline-dark w-50 p-2 mt-3" onClick={"start session"}>
+            Start Matching...
+          </div>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
